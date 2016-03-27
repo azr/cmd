d1c5b8d5bc61805937398f3ad98f42cc03f3b673
Adrien Delorme <adrien.delorme@icloud.com>
2016-03-27 13:51:41 +0200

    doc(varhandler): be more precise with response types

--- varhandler/generator.go
+++ varhandler/generator.go
@@ -1,7 +1,10 @@
 // VarHandler wraps variing http handler funcs
 //
-// Given a func F :
-//  func F([ctx context.Context], x X, y Y, z *Z, zz z.Z) {...}
+// Given a pkg :
+// 	//go:generate varhandler
+//  package server
+//
+//  func F([ctx context.Context], x X, y Y, z *Z, zz z.Z) ([resp interface{},] [status int,] err error) {...}
 //  // and funcs
 //  func HTTPX  (w http.ResponseWriter, r *http.Request) X          {...}
 //  func HTTPY  (w http.ResponseWriter, r *http.Request) (Y, error) {...}
@@ -13,30 +16,52 @@
 //       x := HTTPX(w, r)
 //       y, err := HTTPY(w, r)
 //       if err != nil {
-//       	type HttpErrorInterface interface {
-//       		HttpError() (error string, code int)
-//       	}
-//       	type SelfHttpErrorInterface interface {
-//       		HttpError(w http.ResponseWriter)
-//       	}
-//       	switch t := err.(type) {
-//       	default:
-//       		w.WriteHeader(http.StatusBadRequest)
-//       	case HttpErrorInterface:
-//       		error, code := t.HttpError()
-//       		t.Error(w, error, code)
-//       	case HttpErrorInterface:
-//       		t.HttpError(w)
-//       	}
+//       	HandleHttpErrorWithDefaultStatus(http.StatusBadRequest, err)
 //       	return
 //       }
 //       z := HTTPZ(w, r)
 //       zz := z.HTTPZ(w, r)
-//       F()
-//
-//
+//       resp, status, err := F([context.Background()], x, y, z, zz)
+//       if err != nil {
+//       	HandleHttpErrorWithDefaultStatus(http.InternalServerError, err)
+//       	return
+//       }
+//       if status != 0 { // code generated if status is returned
+//       	w.WriteHeader(status)
+//       }
+//       if resp != nil { // code generated if resp object is returned
+//       	HandleHttpResponse()
+//       }
 //   }
 //
+//   func HandleHttpErrorWithDefaultStatus(status int, err error) {
+//       type HttpError interface {
+//       	HttpError() (error string, code int)
+//       }
+//       type SelfHttpError interface {
+//       	HttpError(w http.ResponseWriter)
+//       }
+//       switch t := err.(type) {
+//       default:
+//       	w.WriteHeader(status)
+//       case HttpError:
+//       	error, code := t.HttpError()
+//       	t.Error(w, error, code)
+//       case HttpError:
+//       	t.HttpError(w)
+//       }
+//   }
+//
+//   func HandleHttpResponse(w http.ResponseWriter, r *http.Request, resp interface{}) {
+//       switch t := resp.(type) {
+//       default:
+//       	// I don't know that type !
+//       case http.Handler:
+//       	t.ServeHTTP(w, r)
+//       case []byte:
+//       	t.Write(t)
+//       }
+//   }
 //
 package main // import "github.com/azr/generators/varhandler"
 
