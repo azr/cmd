37855d697f35e762b0767705e4e7683bc28ffea0
Adrien Delorme <adrien.delorme@icloud.com>
2016-03-27 16:50:01 +0200

    chore(varhandler.examples): generate simple example

--- varhandler/examples/simple_example.go
+++ varhandler/examples/simple_example.go
@@ -6,9 +6,9 @@ func Simple(x X, y Y, z *Z) error {
 	return nil
 }
 
-func HTTPX(w http.ResponseWriter, r *http.Request) X          { return X{} }
-func HTTPY(w http.ResponseWriter, r *http.Request) (Y, error) { return Y{}, nil }
-func HTTPZ(w http.ResponseWriter, r *http.Request) *Z         { return &Z{} }
+func HTTPX(w http.ResponseWriter, r *http.Request) (X, error)  { return X{}, nil }
+func HTTPY(w http.ResponseWriter, r *http.Request) (Y, error)  { return Y{}, nil }
+func HTTPZ(w http.ResponseWriter, r *http.Request) (*Z, error) { return &Z{}, nil }
 
 type X struct {
 }
