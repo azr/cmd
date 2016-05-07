bea5f80f3234b48c540e2f14c1c2ca715e64f999
Adrien Delorme <adrien.delorme@icloud.com>
2016-05-07 10:48:12 +0200

    set example free list size to 42

--- recycler/examples/t.go
+++ recycler/examples/t.go
@@ -1,6 +1,6 @@
 package examples
 
 //go:generate recycler -type=T -output t_pool.go
-//go:generate recycler -type=T -template freelist.gotpl -output t_freelist.go
+//go:generate recycler -type=T -size 42 -template freelist.gotpl -output t_freelist.go
 type T struct {
 }
